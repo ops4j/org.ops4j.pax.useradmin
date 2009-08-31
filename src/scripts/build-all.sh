ffb69801e3e9e7d2f7053063ba496d776fadef50
Matthias Kuespert <mkuespert@googlemail.com>
2009-08-31 13:24:44 +0000

    git-svn-id: https://scm.ops4j.org/repos/ops4j/laboratory/users/mkuespert/useradmin@15999 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- src/scripts/build-all.sh
+++ src/scripts/build-all.sh
@@ -9,9 +9,4 @@ while getopts ":M:P:D:" o ; do
 done
 #
 DEPLOY_SCRIPT=`dirname $0`/deploy-site.sh
-${MAVEN_DIR}mvn ${PROFILE} clean install site && ${DEPLOY_SCRIPT} ${PROFILE} ${DEPLOY_DIR}
-
-
-#DEPLOY_SCRIPT=`dirname $0`/deploy-site.sh
-#echo "running: /java/maven/bin/mvn clean install site && ${DEPLOY_SCRIPT} $1"
-#/java/maven/bin/mvn clean install site && ${DEPLOY_SCRIPT} $1
+${MAVEN_DIR}mvn ${PROFILE} clean install site && ${DEPLOY_SCRIPT} -M${MAVEN_DIR} ${PROFILE} ${DEPLOY_DIR}
