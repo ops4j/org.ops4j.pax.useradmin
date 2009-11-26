85ac1ed3b66e13dc8e4bf2489bcc8fa045bddaf3
Matthias Kuespert <mkuespert@googlemail.com>
2009-11-26 21:06:01 +0000

    Added encryptor
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/useradmin@16586 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-useradmin-service/src/main/java/org/ops4j/pax/useradmin/service/internal/EncryptorImpl.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2009 Matthias Kuespert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.useradmin.service.internal;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * A class to encrypt and verify values based on the <code>java.security</code> API.
+ * 
+ * @see <a href="http://java.sun.com/javase/6/docs/technotes/guides/security/index.html"/>
+ * 
+ * @author Matthias Kuespert
+ * @since 25.11.2009
+ */
+public class EncryptorImpl {
+
+    private MessageDigest m_messageDigest = null;
+
+    private int           m_saltLength    = 0;
+
+    private SecureRandom  m_secureRandom  = null;
+
+    /**
+     * Initializing constructor.
+     * 
+     * @param algorithm The name of the encryption algorithm to use.
+     * @param rngAlgorithm The random-number generator algorithm to use.
+     * @param saltLength The length of the salt.
+     * @throws NoSuchAlgorithmException Thrown if a specified algorithm is not available.
+     */
+    protected EncryptorImpl(String algorithm,
+                            String rngAlgorithm,
+                            String saltLength) throws NoSuchAlgorithmException {
+        m_messageDigest = MessageDigest.getInstance(algorithm);
+        m_saltLength = new Integer(saltLength);
+        m_secureRandom = SecureRandom.getInstance(rngAlgorithm);
+        m_secureRandom.setSeed(System.currentTimeMillis());
+    }
+
+    /**
+     * Creates a new salt for encrypting a value.
+     * 
+     * @return A byte[] containing the new salt.
+     */
+    private byte[] createSalt() {
+        byte[] salt = new byte[m_saltLength];
+        m_secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    /**
+     * Encrypts a value using the given salt.
+     * 
+     * @param value The value to encrypt.
+     * @param salt The salt to use for encryption.
+     * @return A byte[] containing salt and encrypted value.
+     */
+    private byte[] encrypt(byte[] value,
+                           byte[] salt) {
+        m_messageDigest.reset();
+        m_messageDigest.update(salt);
+        m_messageDigest.update(value);
+        //
+        byte[] encryptedValue = m_messageDigest.digest();
+
+        // TODO: check if to encrypt encrypted value over and over again
+        // perform iteration safe is to do more than 1000
+        //        for (int i = 0; i <= 1001; i++) {
+        //            m_messageDigest.reset();
+        //            encryptedValue = m_messageDigest.digest(encryptedValue);
+        //        }
+        //
+        // concatenate salt and encrypted value and return that data
+        byte[] result = new byte[salt.length + encryptedValue.length];
+        System.arraycopy(salt, 0, result, 0, salt.length);
+        System.arraycopy(encryptedValue, 0, result, salt.length, encryptedValue.length);
+        return result;
+    }
+
+    // public interface
+    
+    /**
+     * Encrypts the given value. Creates a new salt on each call.
+     * 
+     * @param value The value to encrypt.
+     * @return A byte[] containing salt and encrypted value.
+     */
+    public byte[] encrypt(byte[] value) {
+        if (null == value) {
+            throw new IllegalArgumentException(UserAdminMessages.MSG_INVALID_VALUE);
+        }
+        byte[] salt = createSalt();
+        return encrypt(value, salt);
+    }
+
+    /**
+     * Checks if the plain input value equals the given encrypted value.
+     * 
+     * @param inputValue The plain-text input value.
+     * @param encryptedValue The encrypted stored value.
+     * @return True if the encrypted input value equals the stored value.
+     */
+    public boolean compare(byte[] inputValue,
+                           byte[] encryptedValue) {
+        // copy salt from stored value
+        byte[] salt = new byte[m_saltLength];
+        System.arraycopy(encryptedValue, 0, salt, 0, m_saltLength);
+        // compare encrypted values
+        byte[] encryptedInputValue = encrypt(inputValue, salt);
+        return MessageDigest.isEqual(encryptedInputValue, encryptedValue);
+    }
+}
